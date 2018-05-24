d7d207ad799197bfdf755d4a9f2dde379dba3a2c
Windows Qiao <342187916@qq.com>
2018-05-24 14:02:13 +0800

    建立模型类并生成数据库表

--- fd_user/models.py
+++ fd_user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    uname=models.CharField(max_length=20)
+    upwd=models.CharField(max_length=40)
+    uemail=models.CharField(max_length=20)
+    urev=models.CharField(max_length=20)
+    uaddress=models.CharField(max_length=100)
+    uphone=models.CharField(max_length=11)
+
