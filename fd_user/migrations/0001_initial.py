97ccff0fecd16c9b1088be16eb3394b884c0a86c
WinDesktop Qiao <342187916@qq.com>
2018-06-04 16:24:15 +0800

    6.4重新上传

--- fd_user/migrations/0001_initial.py
+++ fd_user/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='UserInfo',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('uname', models.CharField(max_length=20)),
                 ('upwd', models.CharField(max_length=40)),
                 ('uemail', models.CharField(max_length=20)),
--- fd_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\qiao6\Desktop\Pycharm-projects_share\FD_OnlineMarket\fd_venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
